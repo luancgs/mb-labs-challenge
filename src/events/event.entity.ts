67de346fd57da6cc95d5c6610ca8013114b3f1d2
Luan Carlos <luan.carlos.3174@gmail.com>
2023-01-20 16:24:02 -0300

    feat: :sparkles: creates event entity

---
+++ src/events/event.entity.ts
@@ -0,0 +1,42 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Admin } from '../admins/admin.entity';
+
+@Entity()
+export class Event {
+  @PrimaryGeneratedColumn('increment', { unsigned: true })
+  id: number;
+
+  @Column({ length: 300 })
+  title: string;
+
+  @Column({ length: 300 })
+  address: string;
+
+  @Column('timestamp')
+  date: Date;
+
+  @Column({ length: 30, nullable: true })
+  contactEmail: string;
+
+  @Column({ length: 15, nullable: true })
+  contactPhone: string;
+
+  @Column({ unsigned: true })
+  availableTickets: number;
+
+  @Column('decimal', { precision: 6, scale: 2 })
+  price: number;
+
+  @OneToOne(() => Admin)
+  @JoinColumn()
+  createdBy: Admin;
+
+  @Column('timestamp')
+  createdAt: Date;
+}
