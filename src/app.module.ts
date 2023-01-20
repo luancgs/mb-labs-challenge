ed26b7381426df528b8aa93323c952ed8afdd27e
Luan Carlos <luan.carlos.3174@gmail.com>
2023-01-20 16:22:52 -0300

    chore: :sparkles: adds typeorm to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { EventsModule } from './events/events.module';
+import { AdminsModule } from './admins/admins.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql-db',
+      port: 3306,
+      username: 'admin',
+      password: 'admin',
+      database: 'MBLABS',
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      synchronize: false,
+    }),
+    UsersModule,
+    EventsModule,
+    AdminsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
