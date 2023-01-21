f88b2f0fd902547e55e1f0274decd22b49a7b662
Luan Carlos <luan.carlos.3174@gmail.com>
2023-01-21 17:53:47 -0300

    chore: :necktie: adds events bussiness logic

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -6,7 +6,12 @@ import {
   Put,
   Delete,
   Param,
+  Query,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
+import { EventCreateError } from './errors/event.create.error';
+import { EventUpdateError } from './errors/event.update.error';
 import { Event } from './event.entity';
 import { EventsService } from './events.service';
 
@@ -15,27 +20,89 @@ export class EventsController {
   constructor(private service: EventsService) {}
 
   @Get()
-  getAll() {
-    return this.service.getEvents();
+  async getAll(
+    @Query('title') title?: string,
+    @Query('organizer') organizer?: string,
+    @Query('address') address?: string,
+    @Query('availableOnly') availableOnly?: string,
+  ) {
+    try {
+      return await this.service.getEvents(
+        title,
+        organizer,
+        address,
+        availableOnly,
+      );
+    } catch (error) {
+      throw new HttpException(
+        `Error: ${error.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   @Get(':id')
-  get(@Param('id') id: number) {
-    return this.service.getEvent(id);
+  async getById(@Param('id') id: number) {
+    try {
+      return await this.service.getEventById(id);
+    } catch (error) {
+      throw new HttpException(
+        `Error: ${error.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   @Post()
-  create(@Body() event: Event) {
-    return this.service.createEvent(event);
+  async create(@Body() event: Event) {
+    try {
+      await this.service.createEvent(event);
+      return 'event created successfully';
+    } catch (error) {
+      if (error instanceof EventCreateError) {
+        throw new HttpException(
+          `Bad Request: ${error.message}`,
+          HttpStatus.BAD_REQUEST,
+        );
+      } else {
+        throw new HttpException(
+          `Error: ${error.message}`,
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
   }
 
   @Put(':id')
-  update(@Param('id') id: number, @Body() event: Partial<Event>) {
-    return this.service.updateEvent(id, event);
+  async update(@Param('id') id: number, @Body() event: Partial<Event>) {
+    try {
+      await this.service.updateEvent(id, event);
+      return 'event updated successfully';
+    } catch (error) {
+      if (error instanceof EventUpdateError) {
+        throw new HttpException(
+          `Bad Request: ${error.message}`,
+          HttpStatus.BAD_REQUEST,
+        );
+      } else {
+        throw new HttpException(
+          `Error: ${error.message}`,
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
   }
 
   @Delete(':id')
-  delete(@Param('id') id: number) {
-    return this.service.deleteEvent(id);
+  async delete(@Param('id') id: number) {
+    try {
+      await this.service.deleteEvent(id);
+      return 'event deleted successfully';
+    } catch (error) {
+      throw new HttpException(
+        `Error: ${error.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
