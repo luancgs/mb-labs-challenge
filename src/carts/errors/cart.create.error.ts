64020d0e34686739da0705cffce061eb58342918
Luan Carlos <luan.carlos.3174@gmail.com>
2023-01-22 09:27:10 -0300

    chore: :goal_net: adds better error handling for carts module

---
+++ src/carts/errors/cart.create.error.ts
@@ -0,0 +1,5 @@
+export class CartCreateError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+}
---
+++ src/carts/errors/cart.delete.error.ts
@@ -0,0 +1,5 @@
+export class CartDeleteError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+}
