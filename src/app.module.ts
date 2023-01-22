661b302a63c149094160846e49af6c2b676626ea
Luan Carlos <luan.carlos.3174@gmail.com>
2023-01-21 23:02:58 -0300

    chore: :wrench: updates modules settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
+import { UsersModule } from './users/users.module';
 import { EventsModule } from './events/events.module';
 import { AdminsModule } from './admins/admins.module';
 import { TicketsModule } from './tickets/tickets.module';
 import { DiscountsModule } from './discounts/discounts.module';
 import { CartsModule } from './carts/carts.module';
+import { OrganizersModule } from './organizers/organizers.module';
+import { UserAuthModule } from './auth/user/user.auth.module';
+import { AdminAuthModule } from './auth/admin/admin.auth.module';
 
 @Module({
   imports: [
@@ -28,6 +31,9 @@ import { CartsModule } from './carts/carts.module';
     TicketsModule,
     DiscountsModule,
     CartsModule,
+    OrganizersModule,
+    AdminAuthModule,
+    UserAuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
