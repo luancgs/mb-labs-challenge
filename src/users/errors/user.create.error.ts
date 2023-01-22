0cd425719359f1a1499f1031d4870935d69e593d
Luan Carlos <luan.carlos.3174@gmail.com>
2023-01-22 01:39:02 -0300

    chore: :goal_net: adds better exception when email already registered

--- src/users/errors/user.create.error.ts
+++ src/users/errors/user.create.error.ts
@@ -1,5 +1,9 @@
 export class UserCreateError extends Error {
   constructor(message: string) {
     super(message);
+
+    if (message.includes('UNQ_email')) {
+      this.message = 'email already registered';
+    }
   }
 }
