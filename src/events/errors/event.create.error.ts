b0ad42b1156ede995238ea11fa2f54e431c2bf21
Luan Carlos <luan.carlos.3174@gmail.com>
2023-01-22 01:42:06 -0300

    chore: :goal_net: adds better exception when no contact info is passed

--- src/events/errors/event.create.error.ts
+++ src/events/errors/event.create.error.ts
@@ -1,5 +1,9 @@
 export class EventCreateError extends Error {
   constructor(message: string) {
     super(message);
+
+    if (message.includes('CHK_contact')) {
+      this.message = 'event must have one or more contact information';
+    }
   }
 }
