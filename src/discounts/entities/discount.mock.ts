b607c23df8ce8ec19cb6ecd6b3ff3b6af3891c00
Luan Carlos <luan.carlos.3174@gmail.com>
2023-01-23 20:40:00 -0300

    test: :clown_face: updates discount mock class to have controller and service mock

--- src/discounts/entities/discount.mock.ts
+++ src/discounts/entities/discount.mock.ts
@@ -1,5 +1,6 @@
 import { AdminMock } from '../../admins/entities/admin.mock';
 import { EventMock } from '../../events/entities/event.mock';
+import { DiscountGetDto } from '../DTOs/discount.get.dto';
 import { Discount } from './discount.entity';
 
 export class DiscountMock extends Discount {
@@ -10,5 +11,13 @@ export class DiscountMock extends Discount {
     this.value = 10;
     this.event = new EventMock(1);
     this.admin = new AdminMock(1);
+  }
+
+  controller(): DiscountGetDto {
+    return new DiscountGetDto(this);
+  }
+
+  service() {
+    return this;
   }
 }
