722d4336bf096f386d363bc08b3b3c2e0e5defe0
Luan Carlos <luan.carlos.3174@gmail.com>
2023-01-23 23:25:22 -0300

    test: :white_check_mark: adds payments module tests

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -5,7 +5,7 @@ import Stripe from 'stripe';
 
 @Injectable()
 export class PaymentService {
-  private stripe: Stripe;
+  stripe: Stripe;
   constructor() {
     this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
       apiVersion: '2022-11-15',
